6f751321f517ce385306cd17a4204f1845df38b0
tristan <yangguochun@ddweilai.com>
2019-03-19 21:41:12 +0800

    update

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export BUILD_DIR=dist
+export BUILD_TYPE=front
+export TARGET_PATH=/data/www/FRONTEND/react-meeting-h5
+
+build_functions=/usr/bin/build-functions
+if [ -f "$build_functions" ]; then
+    . $build_functions
+else
+    npm install --production && npm run build
+fi
