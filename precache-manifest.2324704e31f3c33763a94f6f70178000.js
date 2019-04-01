61e95a129e0d51564c2d854197b732997e9c3183
tristan <yangguochun@ddweilai.com>
2019-04-01 16:39:37 +0800

    Updates

---
+++ precache-manifest.2324704e31f3c33763a94f6f70178000.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9fcb01bd5eb3336490653a447829bb1",
+    "url": "/h5/static/media/point.one.e9fcb01b.png"
+  },
+  {
+    "revision": "121792f1a5d5b912e86b",
+    "url": "/h5/static/js/Arrange.121792f1.chunk.js"
+  },
+  {
+    "revision": "099646380519ba56fe96",
+    "url": "/h5/static/js/EnableGarden.09964638.chunk.js"
+  },
+  {
+    "revision": "3c72893332eb4d58fc36",
+    "url": "/h5/static/js/Legend.3c728933.chunk.js"
+  },
+  {
+    "revision": "57e8c653fcb57f8cf365",
+    "url": "/h5/static/js/PointFour.57e8c653.chunk.js"
+  },
+  {
+    "revision": "ea0d83ca76b7c0b0252f",
+    "url": "/h5/static/js/PointThree.ea0d83ca.chunk.js"
+  },
+  {
+    "revision": "cb4ec82951148fe4887f",
+    "url": "/h5/static/js/PointTwo.cb4ec829.chunk.js"
+  },
+  {
+    "revision": "359a8d2f39fe119cb534",
+    "url": "/h5/static/js/Promises.359a8d2f.chunk.js"
+  },
+  {
+    "revision": "ce9e6ffb1a3502394316",
+    "url": "/h5/static/js/SpeakerTeam1.ce9e6ffb.chunk.js"
+  },
+  {
+    "revision": "96fe43e51b55d841d42d",
+    "url": "/h5/static/js/SpeakerTeam2.96fe43e5.chunk.js"
+  },
+  {
+    "revision": "3f047838f1f322dbd563",
+    "url": "/h5/static/js/Submit.3f047838.chunk.js"
+  },
+  {
+    "revision": "2675bbfa647f4fcb7313",
+    "url": "/h5/static/js/Waiting.2675bbfa.chunk.js"
+  },
+  {
+    "revision": "f6ae723367fbf73365b0",
+    "url": "/h5/static/js/how.f6ae7233.chunk.js"
+  },
+  {
+    "revision": "b8eec107c6dbb10936ecccbe90c20854",
+    "url": "/h5/static/media/modal.bg.b8eec107.png"
+  },
+  {
+    "revision": "b883cde063f3c2bea97d",
+    "url": "/h5/static/js/main.b883cde0.chunk.js"
+  },
+  {
+    "revision": "f0e0e7cbf2fefa431aea",
+    "url": "/h5/static/js/open.f0e0e7cb.chunk.js"
+  },
+  {
+    "revision": "90a1ae60be8d42b36b51",
+    "url": "/h5/static/js/pointone.90a1ae60.chunk.js"
+  },
+  {
+    "revision": "3eefdf85bd2e9ab3cff5",
+    "url": "/h5/static/js/runtime~main.3eefdf85.js"
+  },
+  {
+    "revision": "27acc38af8f2cdeb0228",
+    "url": "/h5/static/js/where.27acc38a.chunk.js"
+  },
+  {
+    "revision": "4ae757f41632dc8c070259190015f335",
+    "url": "/h5/static/media/sign.4ae757f4.png"
+  },
+  {
+    "revision": "72fb0ef0614637fa6990b42ccc4a4abd",
+    "url": "/h5/static/media/15000.72fb0ef0.png"
+  },
+  {
+    "revision": "7062b74ca80f2c34ec201ad4156ff2ad",
+    "url": "/h5/static/media/bgm.7062b74c.mp3"
+  },
+  {
+    "revision": "abbcf0dd22f307f951d88fc1a9ee1215",
+    "url": "/h5/static/media/bg.abbcf0dd.png"
+  },
+  {
+    "revision": "ad63b4727e17248e2641e9ea667ff8fa",
+    "url": "/h5/static/media/bg.orange.ad63b472.png"
+  },
+  {
+    "revision": "cee9cd66870d9bdf45587b92722161e1",
+    "url": "/h5/static/media/wx.avatar.cee9cd66.png"
+  },
+  {
+    "revision": "97e74d59b28c668c06b33a0f19798333",
+    "url": "/h5/static/media/topic.title.97e74d59.png"
+  },
+  {
+    "revision": "adacfa99cc71e2dfbe9d2a69c6010373",
+    "url": "/h5/static/media/how.adacfa99.png"
+  },
+  {
+    "revision": "467156be076d3da7a14772cbba162b11",
+    "url": "/h5/static/media/open.467156be.png"
+  },
+  {
+    "revision": "af9f706f97d80ae657a3",
+    "url": "/h5/static/js/BigMan.af9f706f.chunk.js"
+  },
+  {
+    "revision": "958e9fb3ed0a4012d101e0cea05fcf91",
+    "url": "/h5/static/media/point.two.958e9fb3.png"
+  },
+  {
+    "revision": "6abd9d0db658b503b9a73bfa1921e0e4",
+    "url": "/h5/static/media/point.three.6abd9d0d.png"
+  },
+  {
+    "revision": "bbdf647a600829af05ee111750f613fe",
+    "url": "/h5/static/media/point.four.bbdf647a.png"
+  },
+  {
+    "revision": "7e4fecc7ec286f87f09d775ee9606b5b",
+    "url": "/h5/static/media/bigman.wang.7e4fecc7.png"
+  },
+  {
+    "revision": "03a86ede7b8dfc8d10662e5d6b8c460f",
+    "url": "/h5/static/media/bigman.deng.03a86ede.png"
+  },
+  {
+    "revision": "189f238687c5cd4284ce38979da6c896",
+    "url": "/h5/static/media/bigman.zhang.189f2386.png"
+  },
+  {
+    "revision": "2301c95b0bd480035bf9d908d8e90f33",
+    "url": "/h5/static/media/speaker.dongzc.2301c95b.png"
+  },
+  {
+    "revision": "9a1336c977c53f9b6cbeec8748155470",
+    "url": "/h5/static/media/speaker.zhangyanlong.9a1336c9.png"
+  },
+  {
+    "revision": "fdf091ebdd54f2e79626d08062478b2b",
+    "url": "/h5/static/media/speaker.huangke.fdf091eb.png"
+  },
+  {
+    "revision": "f0e5a80f63afc555579febf204e96aaa",
+    "url": "/h5/static/media/speaker.ms.f0e5a80f.png"
+  },
+  {
+    "revision": "c76de8c49c4ee00a0cd8865d315b6d54",
+    "url": "/h5/static/media/speaker.sq.c76de8c4.png"
+  },
+  {
+    "revision": "1fde0d78a18b02f7ee709b99b14be81d",
+    "url": "/h5/static/media/speaker.zhuxy.1fde0d78.png"
+  },
+  {
+    "revision": "918dbd6879ae678152a7c8cbd8dabd13",
+    "url": "/h5/static/media/speaker.qixi.918dbd68.png"
+  },
+  {
+    "revision": "e02544b99070606f52f72caf3b640e05",
+    "url": "/h5/static/media/speaker.lyx.e02544b9.png"
+  },
+  {
+    "revision": "14ed2aea94f3ab04b3cd9177a9634366",
+    "url": "/h5/static/media/gift.14ed2aea.png"
+  },
+  {
+    "revision": "b3c0990109045a8285d13b166f965bc0",
+    "url": "/h5/static/media/mobile.demo.1.b3c09901.png"
+  },
+  {
+    "revision": "52ae297426ea5573c7a3ba69f6257e8e",
+    "url": "/h5/static/media/mobile.demo.2.52ae2974.png"
+  },
+  {
+    "revision": "a8e4b44cae4e13ec8fbf183b4071ae1a",
+    "url": "/h5/static/media/scene.1.a8e4b44c.jpg"
+  },
+  {
+    "revision": "15c487f535dc893052baf1c82df85cd0",
+    "url": "/h5/static/media/scene.2.15c487f5.jpg"
+  },
+  {
+    "revision": "6d78eb2e7a6bccf5a49b2ae67b29d5c6",
+    "url": "/h5/static/media/scene.3.6d78eb2e.jpg"
+  },
+  {
+    "revision": "37ba57f8b610140095514705a0c55c80",
+    "url": "/h5/static/media/scene.4.37ba57f8.jpg"
+  },
+  {
+    "revision": "ad4ac38871e4da85ea319a36bb4011ec",
+    "url": "/h5/static/media/scene.5.ad4ac388.jpg"
+  },
+  {
+    "revision": "25128e2efd78f129ac1b1e7e94efe031",
+    "url": "/h5/static/media/scene.6.25128e2e.jpg"
+  },
+  {
+    "revision": "312bbb76305dd3d23983",
+    "url": "/h5/static/js/19.312bbb76.chunk.js"
+  },
+  {
+    "revision": "a34f94673d21c6aca257",
+    "url": "/h5/static/js/18.a34f9467.chunk.js"
+  },
+  {
+    "revision": "b883cde063f3c2bea97d",
+    "url": "/h5/static/css/main.74ac4e14.chunk.css"
+  },
+  {
+    "revision": "a95d33f5fcac942b0dd0c0d11f65ca28",
+    "url": "/h5/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-meeting-h5/precache-manifest.a17b4c7c7196f9b6dbfce9d31ecbbbb1.js"
+  "/h5/precache-manifest.2324704e31f3c33763a94f6f70178000.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-meeting-h5/index.html", {
+workbox.routing.registerNavigationRoute("/h5/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
