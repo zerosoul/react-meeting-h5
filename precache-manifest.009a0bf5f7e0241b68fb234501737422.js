e069a27ffd6922612a25ee13883b74dd66903b41
Tristan <yanggc888@163.com>
2019-04-02 22:57:07 +0800

    Updates

---
+++ precache-manifest.009a0bf5f7e0241b68fb234501737422.js
@@ -0,0 +1,226 @@
+self.__precacheManifest = [
+  {
+    "revision": "e9fcb01bd5eb3336490653a447829bb1",
+    "url": "/react-meeting-h5/static/media/point.one.e9fcb01b.png"
+  },
+  {
+    "revision": "b3e28da5e687b4425dbe",
+    "url": "/react-meeting-h5/static/js/Arrange.b3e28da5.chunk.js"
+  },
+  {
+    "revision": "8b6a22c1b2f5317c45b8",
+    "url": "/react-meeting-h5/static/js/EnableGarden.8b6a22c1.chunk.js"
+  },
+  {
+    "revision": "d07f2f55057c5a1eb86d",
+    "url": "/react-meeting-h5/static/js/Legend.d07f2f55.chunk.js"
+  },
+  {
+    "revision": "5c3820c1ced2e1b69a83",
+    "url": "/react-meeting-h5/static/js/PointFour.5c3820c1.chunk.js"
+  },
+  {
+    "revision": "4e4eb6ae7464afa2b11b",
+    "url": "/react-meeting-h5/static/js/PointThree.4e4eb6ae.chunk.js"
+  },
+  {
+    "revision": "d73ab12fccb794d80ec2",
+    "url": "/react-meeting-h5/static/js/PointTwo.d73ab12f.chunk.js"
+  },
+  {
+    "revision": "40d8f548ddeda1ad196f",
+    "url": "/react-meeting-h5/static/js/Promises.40d8f548.chunk.js"
+  },
+  {
+    "revision": "46a0c85638983a60760b",
+    "url": "/react-meeting-h5/static/js/SpeakerTeam1.46a0c856.chunk.js"
+  },
+  {
+    "revision": "2ac7cefdfaeb2738c550",
+    "url": "/react-meeting-h5/static/js/SpeakerTeam2.2ac7cefd.chunk.js"
+  },
+  {
+    "revision": "da81e68c4def468e120a",
+    "url": "/react-meeting-h5/static/js/Submit.da81e68c.chunk.js"
+  },
+  {
+    "revision": "6673a59f94a22cdf32ae",
+    "url": "/react-meeting-h5/static/js/Waiting.6673a59f.chunk.js"
+  },
+  {
+    "revision": "78751476a241b4604800",
+    "url": "/react-meeting-h5/static/js/how.78751476.chunk.js"
+  },
+  {
+    "revision": "b8eec107c6dbb10936ecccbe90c20854",
+    "url": "/react-meeting-h5/static/media/modal.bg.b8eec107.png"
+  },
+  {
+    "revision": "545531b9b2643b392197",
+    "url": "/react-meeting-h5/static/js/main.545531b9.chunk.js"
+  },
+  {
+    "revision": "0bf5223148e3b66b51c4",
+    "url": "/react-meeting-h5/static/js/open.0bf52231.chunk.js"
+  },
+  {
+    "revision": "92d8a9b1019e583c0361",
+    "url": "/react-meeting-h5/static/js/pointone.92d8a9b1.chunk.js"
+  },
+  {
+    "revision": "c8bc623ccbf63ccce86d",
+    "url": "/react-meeting-h5/static/js/runtime~main.c8bc623c.js"
+  },
+  {
+    "revision": "59238b0dc97b6532931a",
+    "url": "/react-meeting-h5/static/js/where.59238b0d.chunk.js"
+  },
+  {
+    "revision": "4ae757f41632dc8c070259190015f335",
+    "url": "/react-meeting-h5/static/media/sign.4ae757f4.png"
+  },
+  {
+    "revision": "72fb0ef0614637fa6990b42ccc4a4abd",
+    "url": "/react-meeting-h5/static/media/15000.72fb0ef0.png"
+  },
+  {
+    "revision": "7062b74ca80f2c34ec201ad4156ff2ad",
+    "url": "/react-meeting-h5/static/media/bgm.7062b74c.mp3"
+  },
+  {
+    "revision": "abbcf0dd22f307f951d88fc1a9ee1215",
+    "url": "/react-meeting-h5/static/media/bg.abbcf0dd.png"
+  },
+  {
+    "revision": "ad63b4727e17248e2641e9ea667ff8fa",
+    "url": "/react-meeting-h5/static/media/bg.orange.ad63b472.png"
+  },
+  {
+    "revision": "cee9cd66870d9bdf45587b92722161e1",
+    "url": "/react-meeting-h5/static/media/wx.avatar.cee9cd66.png"
+  },
+  {
+    "revision": "97e74d59b28c668c06b33a0f19798333",
+    "url": "/react-meeting-h5/static/media/topic.title.97e74d59.png"
+  },
+  {
+    "revision": "adacfa99cc71e2dfbe9d2a69c6010373",
+    "url": "/react-meeting-h5/static/media/how.adacfa99.png"
+  },
+  {
+    "revision": "467156be076d3da7a14772cbba162b11",
+    "url": "/react-meeting-h5/static/media/open.467156be.png"
+  },
+  {
+    "revision": "b021653fdb1a220290c9",
+    "url": "/react-meeting-h5/static/js/BigMan.b021653f.chunk.js"
+  },
+  {
+    "revision": "958e9fb3ed0a4012d101e0cea05fcf91",
+    "url": "/react-meeting-h5/static/media/point.two.958e9fb3.png"
+  },
+  {
+    "revision": "6abd9d0db658b503b9a73bfa1921e0e4",
+    "url": "/react-meeting-h5/static/media/point.three.6abd9d0d.png"
+  },
+  {
+    "revision": "bbdf647a600829af05ee111750f613fe",
+    "url": "/react-meeting-h5/static/media/point.four.bbdf647a.png"
+  },
+  {
+    "revision": "7e4fecc7ec286f87f09d775ee9606b5b",
+    "url": "/react-meeting-h5/static/media/bigman.wang.7e4fecc7.png"
+  },
+  {
+    "revision": "03a86ede7b8dfc8d10662e5d6b8c460f",
+    "url": "/react-meeting-h5/static/media/bigman.deng.03a86ede.png"
+  },
+  {
+    "revision": "189f238687c5cd4284ce38979da6c896",
+    "url": "/react-meeting-h5/static/media/bigman.zhang.189f2386.png"
+  },
+  {
+    "revision": "2301c95b0bd480035bf9d908d8e90f33",
+    "url": "/react-meeting-h5/static/media/speaker.dongzc.2301c95b.png"
+  },
+  {
+    "revision": "9a1336c977c53f9b6cbeec8748155470",
+    "url": "/react-meeting-h5/static/media/speaker.zhangyanlong.9a1336c9.png"
+  },
+  {
+    "revision": "fdf091ebdd54f2e79626d08062478b2b",
+    "url": "/react-meeting-h5/static/media/speaker.huangke.fdf091eb.png"
+  },
+  {
+    "revision": "f0e5a80f63afc555579febf204e96aaa",
+    "url": "/react-meeting-h5/static/media/speaker.ms.f0e5a80f.png"
+  },
+  {
+    "revision": "c76de8c49c4ee00a0cd8865d315b6d54",
+    "url": "/react-meeting-h5/static/media/speaker.sq.c76de8c4.png"
+  },
+  {
+    "revision": "1fde0d78a18b02f7ee709b99b14be81d",
+    "url": "/react-meeting-h5/static/media/speaker.zhuxy.1fde0d78.png"
+  },
+  {
+    "revision": "918dbd6879ae678152a7c8cbd8dabd13",
+    "url": "/react-meeting-h5/static/media/speaker.qixi.918dbd68.png"
+  },
+  {
+    "revision": "e02544b99070606f52f72caf3b640e05",
+    "url": "/react-meeting-h5/static/media/speaker.lyx.e02544b9.png"
+  },
+  {
+    "revision": "14ed2aea94f3ab04b3cd9177a9634366",
+    "url": "/react-meeting-h5/static/media/gift.14ed2aea.png"
+  },
+  {
+    "revision": "b3c0990109045a8285d13b166f965bc0",
+    "url": "/react-meeting-h5/static/media/mobile.demo.1.b3c09901.png"
+  },
+  {
+    "revision": "52ae297426ea5573c7a3ba69f6257e8e",
+    "url": "/react-meeting-h5/static/media/mobile.demo.2.52ae2974.png"
+  },
+  {
+    "revision": "a8e4b44cae4e13ec8fbf183b4071ae1a",
+    "url": "/react-meeting-h5/static/media/scene.1.a8e4b44c.jpg"
+  },
+  {
+    "revision": "15c487f535dc893052baf1c82df85cd0",
+    "url": "/react-meeting-h5/static/media/scene.2.15c487f5.jpg"
+  },
+  {
+    "revision": "6d78eb2e7a6bccf5a49b2ae67b29d5c6",
+    "url": "/react-meeting-h5/static/media/scene.3.6d78eb2e.jpg"
+  },
+  {
+    "revision": "37ba57f8b610140095514705a0c55c80",
+    "url": "/react-meeting-h5/static/media/scene.4.37ba57f8.jpg"
+  },
+  {
+    "revision": "ad4ac38871e4da85ea319a36bb4011ec",
+    "url": "/react-meeting-h5/static/media/scene.5.ad4ac388.jpg"
+  },
+  {
+    "revision": "25128e2efd78f129ac1b1e7e94efe031",
+    "url": "/react-meeting-h5/static/media/scene.6.25128e2e.jpg"
+  },
+  {
+    "revision": "054fbfea0a1b4f6d019e",
+    "url": "/react-meeting-h5/static/js/19.054fbfea.chunk.js"
+  },
+  {
+    "revision": "c935e8e3a4187367d496",
+    "url": "/react-meeting-h5/static/js/18.c935e8e3.chunk.js"
+  },
+  {
+    "revision": "545531b9b2643b392197",
+    "url": "/react-meeting-h5/static/css/main.5ca2c61c.chunk.css"
+  },
+  {
+    "revision": "c5f6aa335b23ad134fe1da056ff51458",
+    "url": "/react-meeting-h5/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/h5/precache-manifest.2324704e31f3c33763a94f6f70178000.js"
+  "/react-meeting-h5/precache-manifest.009a0bf5f7e0241b68fb234501737422.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/h5/index.html", {
+workbox.routing.registerNavigationRoute("/react-meeting-h5/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
